c3f2cdd5592e3dcea68772cbe28df39e25054935
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2015-05-07 21:19:43 -0700

    CMessUp-> Practicing with some examples.

--- Levels/CMessUp/levelSelect.js
+++ Levels/CMessUp/levelSelect.js
@@ -1,29 +1,13 @@
-// Creates an element called Canvas, and passes it as a 2D element.
-var canvas = document.createElement("canvas");
-var context = canvas.getContext("2d");
 
-//Sets the width and height of the canvas.
-canvas.width = 512;
-canvas.height = 512;
-
-// Sets canvas as a child of the body.
-document.body.appendChild(canvas);
-
-// Set a variable to check if the image is loaded.
-var backReady = 'false';
-
-// Sets the var backImage to be a new Image, and creates a function to check to see if it is loaded.
-var backImage = new Image();
-backImage.onload = function () {
-    backReady = true;
-}
-
-// Sets the backImage to be an image.
-backImage.src = "images/background_star.png";
-
-// Creates a function to draw the background.
-var render = function () {
-    if (bgReady) {
-        context.drawImage(backImage, 0, 0);
+    function changeImage() {
+        var image = document.getElementById('bucket');
+        if(image.src.match("bucketwater")){
+            image.src = "../../Justin%20Test/bucketempty.png"
+        }else{
+            image.src = "../../Justin%20Test/bucketwater.png"
+        }
     }
-}
+    function fontSizeUp() {
+        var fontEnlarge = document.getElementById("paragraph");
+        fontEnlarge.style.fontSize = "25px";
+    }
