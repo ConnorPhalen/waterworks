689fa84342d435678fbf2f59ccd673c552787ffb
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2015-05-08 12:12:09 -0700

    Greg testing Level select -> Messed round and tried to get the text to display on the images. level1 -> Cleaned p some junk code from when I tried to set the interval on page load.

--- Levels/level1.js
+++ Levels/level1.js
@@ -1,34 +1,41 @@
 var time = 0;
 var TIME_MAX = 10;
 
-//
+// Checks to see if time has reached its max limit.
 function waterTimer(){
     if(time >= TIME_MAX){
         alert("You Lose!");
     }
 }
 
-// timeAdd 
+// Increments time by 1, and starts waterTimer().
 function timeAdd(){
     time++;
     waterTimer();
 }
 
+// Tried to get the page to set an interval when it loaded.
 window.onload = function () {
-    document.getElementById('waterTest').onclick = function () {
         if (time < 1) {
             setInterval(timeAdd(), 1000);
         } else {
             clearInterval(time);
             time = 0;
         }
-    }
+    
 }
 
+// Rises from the shadows of JavaScript...
 function batman(){
     alert("Batman!");
 }
+
+// Back snap in 3...2...1...
+function bane(){
+    alert("You mearly adopted the darkness, I... was born in it.");
+}
+
 // Sets the timeAdd() function to go off once a second.
 function test() {
-    setInterval(timeAdd(), 1000);
+    bucket.setInterval(function(){time++; waterTimer();}, 1000);
 }
