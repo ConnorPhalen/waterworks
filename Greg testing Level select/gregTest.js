ca6f4633d72fc72dfac06297a6a6c91f0ee572f1
gawg02 <g_dog02@hotmail.com>
2015-05-08 12:58:08 -0700

    things

--- Greg testing Level select/gregTest.js
+++ Greg testing Level select/gregTest.js
@@ -1,1 +1,6 @@
 var firstBucket = document.getElementById("emptyBucket0");
+var audio = new audio(../sounds/water_click.wav);
+var back = document.getElementById("homeScreens");
+sound1:function(){
+    audio.play();
+}
