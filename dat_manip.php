6cacc46d1ca4f1c4445d2248870c5ccccc053ffa
Francis <sonsofix@yahoo.ca>
2015-05-13 23:08:44 -0700

    Francis deliverables. With love <3

--- dat_manip.php
+++ dat_manip.php
@@ -1,23 +1,51 @@
 <?php
     $name = $_GET["usn"];
     $score = $_GET["s"];
+    $new = "no";
 
-    $file = fopen("dat.txt", "r") or die("Unable to open file!");
+    if(strlen($name) > 5) {
+        
+        if (!is_numeric($score)) {
 
+            die("Score is not a number.");
+            
+        } 
 
+    } else {
+        
+        die("Name can not be less than 5 characters.");
 
+    }
 
+    foreach (file("dat.txt") as $line) {
 
+         $match = FALSE;
 
+         if (strcmp($name, substr($line, 0, strpos($line, ';'))) == 0) {
+             
+              $match = TRUE;
+             
+              break;
+         } 
+         
+    }
 
+    if (!$match) {
+        
+        $new = "yes";
 
-    //if match do
-    $file = fopen("dat.txt", "a+") or die("Unable to open file!");
-    //get score datum
-    //set name to name and set score to datum
-    //else
-    echo fwrite($file, "$name;$score<br/>");
+        $dat = fopen("dat.txt", "a+") or die("Unable to open file!");
+    
+        echo fwrite($dat, "$name;$score\n");
 
-    fclose($file);
-    header("Location: http://www.doooge.netau.net/prgTst/dataTest.html?usn=$user&s=$score&reg=true");
+        fclose($dat);
+
+    } else {
+
+        $score = substr($line, strpos($line, ';') + 1);
+
+    }  
+
+    header("Location: http://www.doooge.netau.net/prgTst/dataTest.html?reg=true&ndat=$new");
+    die();
 ?>
