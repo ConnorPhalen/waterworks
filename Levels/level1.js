07d1ba0b66266e3c819c452fbf2f4b9190390490
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2015-05-13 19:02:23 -0700

    level1.js 0> Made the "wave" collide with the gate and then stop. It is fixed, and I do not want to talk about it.

--- Levels/level1.js
+++ Levels/level1.js
@@ -5,14 +5,12 @@ var timeOverflow = 0;
 var TIMEOVERFLOW_MAX = 2;
 var timerDelay = 33;
 var waveMovement = 4;
-var mainTimer;
-
+var mainTimer = setInterval(function () { timeAdd();}, timerDelay);
 // Checks to see if time has reached its max limit.
 function loseCheck(){
     if(testCollision('batman', 'gateImg')){
         clearInterval(mainTimer);
-        alert("You Lose!");
-    }
+     }
 }
 
 // Increments the timer and moves the image. timeOverflow basically slows things down by a division of X.
@@ -24,6 +22,7 @@ function timeAdd(){
         time++;
         move();
     }
+    loseCheck();
 }
 
 // Grabs the picture ID, and its style value of left, and adds x px.
@@ -34,14 +33,14 @@ function move() {
 }
 
 // Page loads, then starts a timer that moves and increments values with a delay of timerDelay.
-window.onload = function () {
+/*window.onload = function () {
         if (time < 1) {
-           mainTimer = setInterval(function () { timeAdd();loseCheck();}, timerDelay);
+          var mainTimer = setInterval(function () { timeAdd();}, timerDelay);
         } else {
             clearInterval(mainTimer);
             time = 0;
         }  
-}
+}*/
 
 // ---------------------- Function to test out collisions between images. Best used with more rectangular images.
 function testCollision(objectA, objectB){
@@ -71,6 +70,7 @@ function testCollision(objectA, objectB){
     // If both X and Y have collided, then a collision has occured.
     if(collideX && collideY){
         alert("Collision!");
+        return true;
         // document.getElementById(objectB).id = 'dead';
     }
 }
