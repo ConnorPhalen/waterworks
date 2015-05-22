83d29ab9699017972898f91effb5554ecddb9e9c
gawg02 <g_dog02@hotmail.com>
2015-05-21 20:18:29 -0700

    did very little

--- firstLevel.js
+++ firstLevel.js
@@ -20,7 +20,6 @@ var turnsMax = 7;
 var scoreReduction = 100;
 var tankMax = parseInt(document.getElementById('fillTo').innerHTML);
 var tankCurrent = 0;
-
 // Checks to see if time has reached its max limit.
 function loseCheck(){
     if(testCollision('wave', 'gateImg')){
